0e1f6d3fa280bc8597e39fe16703e04a0a01ff51
Klementiy <74532062+Klementiy@users.noreply.github.com>
2020-11-19 21:41:25 +0300

    V1

---
+++ GoRacing/GoRacing/GoRacing/ChangeCarCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  ChangeCarCollectionViewCell.swift
+//  GoRacing
+//
+//  Created by user on 19/11/2020.
+//
+
+import UIKit
+
+class ChangeCarCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var carPicture: UIImageView!
+    
+}
---
+++ GoRacing/GoRacing/GoRacing/ResultTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  ResultTableViewCell.swift
+//  GoRacing
+//
+//  Created by user on 19/11/2020.
+//
+
+import UIKit
+
+class ResultTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var namePlayer: UILabel!
+    @IBOutlet weak var gameResult: UILabel!
+    @IBOutlet weak var timeResult: UILabel!
+    
+    }
+
+
